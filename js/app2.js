182f2edd4a19101d3986bcec7e8d01a8f54e876a
anwarmoazam <anwar.palimarwar@gmail.com>
2023-06-07 16:54:34 +0530

    da and other fields added

--- js/app2.js
+++ js/app2.js
@@ -1,7 +1,6 @@
 // Module for handling data manipulation
 const dataModule = (function () {
     const npaRate = 20;
-    const daRate = [{date: new Date(30,06,2017),rate:4},{date: new Date(31,12,2017),rate:5},{date: new Date(28,02,2018),rate:7}];
     const data = JSON.parse(localStorage.getItem('data')) || [];
 
     function getDaysInMonth(month, year) {
@@ -14,15 +13,42 @@ const dataModule = (function () {
     }
 
     return {
-        saveData: function (name, salary, npa, dateValue) {
+        saveData: function (name, salary, npa, dateValue, washing) {
             const obj = {};
             const date = new Date(dateValue);
             const totalDays = daysRemainingInMonth(date);
             obj.name = name.trim();
             obj.salary = Number(salary);
             obj.npa = npa;
+            obj.washing = washing;
             const npaAmountPerDay = ((obj.salary * npaRate / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            const washingAllowancePerDay = (150 / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            obj.washingAllowance = washing;
+            let daAmountPerDay;
+            if (date >= new Date(2017, 00, 01) && date <= new Date(2017, 05, 30)) {
+                daAmountPerDay = ((obj.salary * 4 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2017, 06, 01) && date <= new Date(2017, 11, 31)) {
+                daAmountPerDay = ((obj.salary * 5 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2018, 00, 01) && date <= new Date(2018, 05, 30)) {
+                daAmountPerDay = ((obj.salary * 7 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2018, 06, 01) && date <= new Date(2018, 11, 31)) {
+                daAmountPerDay = ((obj.salary * 9 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2019, 00, 01) && date <= new Date(2019, 05, 30)) {
+                daAmountPerDay = ((obj.salary * 12 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2019, 06, 01) && date <= new Date(2021, 05, 30)) {
+                daAmountPerDay = ((obj.salary * 17 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2021, 06, 01) && date <= new Date(2021, 11, 31)) {
+                daAmountPerDay = ((obj.salary * 31 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2022, 00, 01) && date <= new Date(2022, 05, 30)) {
+                daAmountPerDay = ((obj.salary * 34 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2022, 06, 01) && date <= new Date(2022, 11, 31)) {
+                daAmountPerDay = ((obj.salary * 38 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            } else if (date >= new Date(2023, 00, 01) && date <= new Date(2023, 05, 30)) {
+                daAmountPerDay = ((obj.salary * 42 / 100) / getDaysInMonth(date.getMonth() + 1, date.getFullYear())).toFixed(2);
+            }
             obj.totalNPAAmount = (obj.npa === 'yes') ? Math.round(totalDays * npaAmountPerDay) : 0;
+            obj.washingAllowance = (obj.washing === 'yes') ? Math.round(totalDays * washingAllowancePerDay) : 0
+            obj.daAmount = Math.round(totalDays * daAmountPerDay);
             obj.days = totalDays;
             obj.month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date) + "/" + date.getFullYear();
             data.push(obj);
@@ -32,9 +58,9 @@ const dataModule = (function () {
         getData: function () {
             return data;
         },
-        deleteData: function(index){
-            data.splice(index,1);
-            localStorage.setItem('data',JSON.stringify(data));
+        deleteData: function (index) {
+            data.splice(index, 1);
+            localStorage.setItem('data', JSON.stringify(data));
         }
     }
 })();
@@ -42,14 +68,15 @@ const dataModule = (function () {
 // Module for handling UI related tasks
 const uiModule = (function () {
     const tableBodyData = document.querySelector('tbody');
-    
+
     return {
         getDOM: function () {
             return {
                 name: document.getElementById('name').value,
                 salary: document.getElementById('salary').value,
                 npa: document.getElementById('npa').value,
-                date: document.getElementById('date').value
+                date: document.getElementById('date').value,
+                washing: document.getElementById('washing').value,
             };
         },
         addRow: function (entry, index) {
@@ -62,19 +89,21 @@ const uiModule = (function () {
                 <td>${entry.days}</td>
                 <td>${entry.salary}</td>
                 <td>${entry.totalNPAAmount}</td>
-                <td>${entry.salary+entry.totalNPAAmount}</td>
+                <td>${entry.washingAllowance}</td>
+                <td>${entry.daAmount}</td>
+                <td>${entry.salary + entry.daAmount + entry.totalNPAAmount + entry.washingAllowance}</td>
                 <td><button class="delete-btn" data-id="${row.id}">Delete</button></td>
             `;
             tableBodyData.appendChild(row);
         },
-        deleteRow: function(id){
+        deleteRow: function (id) {
             const row = document.getElementById(id);
             tableBodyData.removeChild(row);
         },
         populateTable: function () {
             const data = JSON.parse(localStorage.getItem('data')) || [];
             data.forEach((item, index) => {
-                console.log(item,index);
+                console.log(item, index);
                 this.addRow(item, index);
             });
         }
@@ -87,11 +116,11 @@ const appModule = (function (dataCtrl, uiCtrl) {
     document.getElementById('form').addEventListener('submit', function (event) {
         event.preventDefault();
         const inputData = uiCtrl.getDOM();
-        const newData = dataCtrl.saveData(inputData.name, inputData.salary, inputData.npa, inputData.date);
+        const newData = dataCtrl.saveData(inputData.name, inputData.salary, inputData.npa, inputData.date, inputData.washing);
         uiCtrl.addRow(newData, dataCtrl.getData().length - 1);
     });
-    document.querySelector('tbody').addEventListener('click',function(event){
-        if(event.target.classList.contains('delete-btn')){
+    document.querySelector('tbody').addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete-btn')) {
             const row = event.target.parentElement.parentElement;
             const index = Array.from(row.parentElement.children).indexOf(row);
             uiCtrl.deleteRow(event.target.dataset.id);
